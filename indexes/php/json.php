7b5c355ff624479ded3425a974ab3ff724a3ab70
bigeppo <carlog91cast@gmail.com>
2022-08-25 18:46:55 +0200

    set folders

--- indexes/php/json.php
+++ indexes/php/json.php
@@ -1,6 +1,16 @@
 <!-- in questa pagina creo il database da richiamare con chiamata axios.get -->
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
 
     $albums = [
         [
@@ -75,4 +85,7 @@
         ]
     ];
 
-?>
+    ?>
+</body>
+
+</html>
